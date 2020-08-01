ebb24aa03b69eed4f923682a21b20015c03c40bf
satishbhutagadda <sathishbhutagadda@gmail.com>
2020-08-01 17:36:15 +0530

    Added routing and axios in the application. implemented CRUD operations using jsonplaceholder API.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './pages/About';
 import Header from './components/layout/Header';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 
 class App extends Component {
+  APIURL = 'https://jsonplaceholder.typicode.com/todos';
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        {
-          id: 1,
-          title: 'Learn React as soon as possible!',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Learn Node js after React!',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Try read complete JavaScript.Info site!',
-          completed: false
-        },
-      ]
+      todos: []
     };
+  }
+
+  componentDidMount() {
+    axios.get(`${this.APIURL}?_limit=4`).then((response) => {
+      console.log(response.data);
+      this.setState({ todos: response.data })
+    })
   }
 
   // Toggle Checkbox
@@ -43,6 +38,13 @@ class App extends Component {
 
   // Delete Todo
   deleteTodo = (id) => {
+    axios.delete(`${this.APIURL}/${id}`).then((response) => {
+      this.setState({
+        todos: this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      })
+    })
     this.setState({
       todos: this.state.todos.filter(todo => {
         return todo.id !== id;
@@ -51,21 +53,29 @@ class App extends Component {
   }
 
   addTodo = (title) => {
-    const todo = {
-      id: this.state.todos.length + 1,
+    const newTodo = {
       title,
       completed: false
     }
-    return this.setState({todos: [...this.state.todos, todo]})
+    axios.post(this.APIURL, newTodo).then((response) => {
+      this.setState({ todos: [...this.state.todos, response.data] })
+    })
   }
 
   render() {
     return (
-      <div className="Todo-App">
-        <Header/>
-        <AddTodo addTodo={this.addTodo}/>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
-      </div>
+      <Router>
+        <div className="Todo-App">
+          <Header />
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo} />
+              <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
+            </React.Fragment>
+          )} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
     );
   }
 }
